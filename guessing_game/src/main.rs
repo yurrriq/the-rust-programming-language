590e668460368408df18f5fa4bf8dba447cd6d0a
Eric Bailey <eric@ericb.me>
2015-09-24 02:05:27 -0500

    Add secret_number to guessing_game
    - thread-local
    - 1 to 100
    - printed temporarily, for debugging

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,23 @@
+extern crate rand; // use rand from dependecies in Cargo.toml
+
 use std::io;
+use rand::Rng; // Rng for "range"
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    // `thread_rng()` => thread-local
+    // gen_range(lower_inclusive, upper_exclusive)
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();        // Strings are mutable and UTF-8
-                                          // ::f() =>  type-level function
+                                          // `::f()` =>  type-level function
     io::stdin().read_line(&mut guess)     // `io::stdin()` needs `use std::io;`
-        .ok()                             // & => reference
+        .ok()                             // `&` => reference
         .expect("Failed to read line");   // references are immutable by default
 
     // `io::stdin()` returns an `io::Result`
