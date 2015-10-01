6f7e8cd7d02fb73193ce4c0e79c82061981cbbf6
Eric Bailey <eric@ericb.me>
2015-10-01 03:07:20 -0500

    Make eating take a second

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 struct Philosopher {
     name: String
 }
@@ -13,6 +15,8 @@ impl Philosopher {
     }
 
     fn eat(&self) {
+        println!("{} is eating.", self.name);
+        thread::sleep_ms(1000);
         println!("{} is done eating.", self.name)
     }
 }
